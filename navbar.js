28d1f1c9750e6694101203f4e129a307e12997b6
lorenzo <56167164+Loenus@users.noreply.github.com>
2022-12-08 18:29:51 +0100

    Improve Navbar

---
+++ navbar.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    const navbar = document.getElementById("navbar");
+    const kembiki_link = document.getElementById("kembiki_link");
+    const kembiki_logo = document.getElementById("kembiki_logo");
+    const instagram_link = document.getElementById("instagram_link");
+    const instagram_logo = document.getElementById("instagram_logo");
+    
+    let breakpoint = 500;
+
+    kembiki_link.style.visibility = "hidden";
+    kembiki_logo.style.opacity = 0;
+    instagram_link.style.visibility = "hidden";
+    instagram_logo.style.opacity = 0;
+    window.addEventListener("scroll", function() {
+        var scrollDown = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        if (scrollDown >= breakpoint) {
+            navbar.classList.add("navbar-filled");
+
+            kembiki_link.style.visibility = "visible";
+            kembiki_logo.style.opacity = 1;
+            instagram_link.style.visibility = "visible";
+            instagram_logo.style.opacity = 1;
+        } else {
+            if (navbar.classList.contains("navbar-filled"))
+                navbar.classList.remove("navbar-filled");
+            
+            kembiki_link.style.visibility = "hidden";
+            kembiki_logo.style.opacity = 0;
+            instagram_link.style.visibility = "hidden";
+            instagram_logo.style.opacity = 0;
+        }
+    })
+}
