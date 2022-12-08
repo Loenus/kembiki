d79dfc14d6f6cb807c72ae4fcf3126de7c116e5c
Loenus <lollo.diaspro@gmail.com>
2022-12-08 21:23:03 +0100

    Progress navbar

--- navbar.js
+++ navbar.js
@@ -2,15 +2,13 @@ window.onload = function() {
     const navbar = document.getElementById("navbar");
     const kembiki_link = document.getElementById("kembiki_link");
     const kembiki_logo = document.getElementById("kembiki_logo");
-    const instagram_link = document.getElementById("instagram_link");
-    const instagram_logo = document.getElementById("instagram_logo");
+    //const instagram_link = document.getElementById("instagram_link");
+    //const instagram_logo = document.getElementById("instagram_logo");
     
-    let breakpoint = 500;
+    let breakpoint = window.innerHeight / 2;
 
     kembiki_link.style.visibility = "hidden";
     kembiki_logo.style.opacity = 0;
-    instagram_link.style.visibility = "hidden";
-    instagram_logo.style.opacity = 0;
     window.addEventListener("scroll", function() {
         var scrollDown = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         if (scrollDown >= breakpoint) {
@@ -18,16 +16,12 @@ window.onload = function() {
 
             kembiki_link.style.visibility = "visible";
             kembiki_logo.style.opacity = 1;
-            instagram_link.style.visibility = "visible";
-            instagram_logo.style.opacity = 1;
         } else {
             if (navbar.classList.contains("navbar-filled"))
                 navbar.classList.remove("navbar-filled");
             
             kembiki_link.style.visibility = "hidden";
             kembiki_logo.style.opacity = 0;
-            instagram_link.style.visibility = "hidden";
-            instagram_logo.style.opacity = 0;
         }
     })
 }
